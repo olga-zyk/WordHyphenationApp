e6a9f680468d59dcfb15eae4f980fd515e8f83c9
Olga Zyk <my.agleh@gmail.com>
2018-08-07 19:49:42 +0300

    Add possibility for user to save output file to local machine; Add time measurement;

--- batch_mode.py
+++ batch_mode.py
@@ -1,5 +1,7 @@
 from hyphenation_alg import Hyphenator
 from strategy_interface import StrategyInterface
+import time
+import os.path
 
 
 class BatchMode(StrategyInterface):
@@ -11,10 +13,25 @@ class BatchMode(StrategyInterface):
         file.close()
 
         batch = Hyphenator(self.config)
+
+        hyphenated_words = []
+        start_time = time.time()
         for word in data:
             word = batch.hyphenate(word)
+            hyphenated_words.append(word)
+        end_time = time.time()
+        time_diff = end_time - start_time
 
-            with open('hyphenated.txt', 'a+') as file:
-                file.write(word + '\n')
+        formatted_string = '\n'.join(hyphenated_words)
 
-        print('The words were hyphenated and saved to the file. Please, check the output file.')
+        path_to_file = input('Type in the full path to output folder: ').strip()
+        doc_title = input('Type in the title for output file: ').strip()
+        complete_name = os.path.join(path_to_file, doc_title + '.txt')
+
+        file = open(complete_name, 'w+')
+        file.write(formatted_string)
+        file.close()
+
+        print('The hyphenated words were successfully saved to the file. Please, check the output file.')
+        print('The running time is: {0} seconds'.format(round(time_diff, 5)))
+
