cd42990cfa7fd339a2e7646f0d170e1f9613c415
Olga Zyk <my.agleh@gmail.com>
2018-08-07 19:43:24 +0300

    Add time measuring

--- interactive_mode.py
+++ interactive_mode.py
@@ -1,11 +1,19 @@
 from hyphenation_alg import Hyphenator
 from strategy_interface import StrategyInterface
+import time
 
 
 class InteractiveMode(StrategyInterface):
 
     def execute(self):
         word = input('Type in the word for hyphenation: ').strip()
+
         interactive = Hyphenator(self.config)
+
+        start_time = time.time()
         result = interactive.hyphenate(word)
-        print('before: {0}\nafter: {1}'.format(word, result))
+        end_time = time.time()
+        time_diff = end_time - start_time
+
+        print('before: {0}\nafter: {1}'.format(word, result) + '\n')
+        print('The running time is: {0} seconds'.format(round(time_diff, 5)))
